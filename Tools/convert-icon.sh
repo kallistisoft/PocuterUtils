a189667cd90a88a1afe4b308ab0040ac21361f22
Kallistisoft (Hail Eris!) <77658107+kallistisoft@users.noreply.github.com>
2023-01-15 22:14:35 -0600

    change: convert-icon script to probe it's own name remove: log mockup file from code uploader project

--- Tools/convert-icon.sh
+++ Tools/convert-icon.sh
@@ -3,27 +3,29 @@
 # Copyright 2022 Kallistisoft
 # MIT License -- https://opensource.org/licenses/MIT
 
+SELF=$(basename $0);
+
 # show: help usage info
 if [[ -z "$1" || "$1" == '--help' ]]; then
-	echo "USAGE: $(basename $0) FILENAME"
+	echo "USAGE: $SELF FILENAME"
 	exit 1
 fi
 
 # test: input file exists
 if [[ ! -e "$1" ]]; then
-	echo "convert-icon: unable to open file [$1]"
+	echo "$SELF: unable to open file [$1]"
 	exit 1
 fi
 
 # test: imagemagick convert utility is installed
 if [[ -z $(which convert) ]]; then
-	echo "convert-icon: unable to find the imagemagick 'convert' utility..."
+	echo "$SELF: unable to find the imagemagick 'convert' utility..."
 	exit 1
 fi
 
 # test: base64 encode utility is installed
 if [[ -z $(which base64) ]]; then
-	echo "convert-icon: unable to find the 'base64' utility..."
+	echo "$SELF: unable to find the 'base64' utility..."
 	exit 1
 fi
 
@@ -33,13 +35,13 @@ convert $1 -channel RGBA -background black -alpha remove -alpha off $1.rgba
 # test: output file created and convert to base64
 if [[ -e "$1.rgba" ]]; then
 	base64 $1.rgba -w0 > $1.base64
-	echo "convert-icon: converted [$1] to base64 string [$1.base64]"
+	echo "$SELF: converted [$1] to base64 string [$1.base64]"
 	rm $1.rgba
 	exit 0
 fi
 
 # error: problem with imagemagick conversion
-echo "convert-icon: error converting input file [$1]"
+echo "$SELF: error converting input file [$1]"
 exit 1
 
 
